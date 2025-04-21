6ab3d349032e10f248568cdb38ef68e1d28c9aba
Pravallika583 <pravallika.peddi12@gmail.com>
2025-04-21 20:53:42 +0000

    add delete functionality, responsive layout, search filter, and show/hide password toggle

--- src/components/PasswordItem/index.js
+++ src/components/PasswordItem/index.js
@@ -1,29 +1,39 @@
 import './index.css'
 
 const PasswordItem = props => {
-  const {passwordDetails} = props
-  const {id, website, username, password} = passwordDetails
+  const {passwordDetails, onDeletePasswordItem, showPasswords} = props
+  const {id, website, username, password, logoBgColor} = passwordDetails
   return (
-    <li className="eachpassword-details">
-      <div className="password-item">
-        <div className="logo-details">
-          <div className="initial-circle">
-            <p className="logo">{website[0].toUpperCase()}</p>
-          </div>
-          <div className="details">
-            <p className="website">{website}</p>
-            <p className="username">{username}</p>
-            <p className="password">{password}</p>
-          </div>
+    <li className="password-item">
+      <div className="logo-details">
+        <div className="initial-circle" style={{backgroundColor: logoBgColor}}>
+          <p className="logo">{website[0].toUpperCase()}</p>
         </div>
-        <button data-testid="delete" className="delete-button">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
-            alt="delete"
-            className="delete-icon"
-          />
-        </button>
+        <div className="details">
+          <p className="website">{website}</p>
+          <p className="username">{username}</p>
+          {showPasswords ? (
+            <p className="password">{password}</p>
+          ) : (
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/password-manager-stars-img.png"
+              alt="stars"
+              className="stars-image"
+            />
+          )}
+        </div>
       </div>
+      <button
+        data-testid="delete"
+        className="delete-button"
+        onClick={onDeletePasswordItem}
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
+          alt="delete"
+          className="delete-icon"
+        />
+      </button>
     </li>
   )
 }
